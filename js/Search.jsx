392b7f4841ea67a7a03c4dbfe8f87d8f6f0807fd
theasteve <steven.aguilar@stu.bmcc.cuny.edu>
2018-04-11 18:49:43 -0400

    managing state

---
+++ js/Search.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import preload from '../data.json'
+import ShowCard from './ShowCard'
+
+const Search = () => (
+  <div className="search">
+    <div>
+      {preload.shows.map(show => <ShowCard key={show.imdbID} {...show}/>)};
+    </div>
+  </div>
+);
+
+
+export default Search;
