db072f6e75cae223cc09e0c34c18d6380d50ef92
theasteve <steven.aguilar@stu.bmcc.cuny.edu>
2018-04-13 16:46:46 -0400

    Create logic for search bar

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import preload from '../data.json'
 import ShowCard from './ShowCard'
 
-const Search = () => (
-  <div className="search">
-    <div>
-      {preload.shows.map(show => <ShowCard key={show.imdbID} {...show}/>)};
-    </div>
-  </div>
-);
+class Search extends Component {
+  state = {
+    searchTerm: ' '
+  };
+  handleSearchTermChange = event => {
+    this.setState({searchTerm: event.target.value});
+  }
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>{ this.state.searchTerm }</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value = {this.state.searchTerm}
+            type="text"
+            placeholder="search" />
+        </header>
+        <div>
+          {preload.shows
+            .filter(
+              show =>
+              `${show.title} ${show.description}`.toUpperCase().indexOf(this
+                .state.searchTerm.toUpperCase()) >= 0
+              )
+            .map(show => <ShowCard key={show.imdbID} {...show}/>)};
+        </div>
+      </div>
+    );
+  }
+}
+
 
 
 export default Search;
